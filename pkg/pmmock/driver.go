a471502306250e3c83ce19d386e5eb9e7bb2cd35
MOZGIII <mike-n@narod.ru>
2021-02-19 01:15:10 +0300

    Add Auto mode to mock driver

--- pkg/pmmock/driver.go
+++ pkg/pmmock/driver.go
@@ -1,6 +1,7 @@
 package pmmock
 
 import (
+	"net"
 	"time"
 
 	"github.com/MOZGIII/port-map-operator/pkg/portmap"
@@ -28,4 +29,30 @@ func (ctl *Control) Inject(res *portmap.Response, timeout time.Duration) {
 	case <-time.After(timeout):
 		panic("timeout while injecting response")
 	}
+}
+
+func (ctl *Control) Auto() chan<- struct{} {
+	stopch := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-stopch:
+				return
+			case ctlreq := <-ctl.RequestCh:
+				// req := ctlreq.Request
+				res := &portmap.Response{
+					Protocol:    ctlreq.Request.Protocol,
+					NodePort:    ctlreq.Request.NodePort,
+					GatewayPort: ctlreq.Request.GatewayPort,
+					GatewayIP:   net.IPv4(1, 0, 0, 0),
+					Lifetime:    ctlreq.Request.Lifetime,
+				}
+				ctl.ResponseCh <- ResponseWrap{
+					Response: res,
+					Error:    nil,
+				}
+			}
+		}
+	}()
+	return stopch
 }
