961d7b0761a047cae5aaa905f973bbefefb6cbb9
MOZGIII <mike-n@narod.ru>
2021-02-19 11:02:27 +0300

    Vendor executable dependencies

--- go.mod
+++ go.mod
@@ -10,4 +10,6 @@ require (
 	k8s.io/apimachinery v0.20.2
 	k8s.io/client-go v0.20.2
 	sigs.k8s.io/controller-runtime v0.8.2
+	sigs.k8s.io/controller-tools v0.4.1
+	sigs.k8s.io/kustomize/kustomize/v3 v3.10.0
 )
--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,8 @@ package tools
 
 import (
 	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+	_ "sigs.k8s.io/kustomize/kustomize/v3"
 )
 
 // This file imports packages that are used when running go generate, or used
